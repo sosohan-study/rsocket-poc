c4211eee71805525a3c3e050f81670069471d0a7
김우재 <kwj1270@estsoft.com>
2022-08-29 16:39:12 +0900

    feat(kwj120): initialize project

---
+++ rsocket-client/src/test/java/com/poc/rsocketclient/RsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.rsocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
