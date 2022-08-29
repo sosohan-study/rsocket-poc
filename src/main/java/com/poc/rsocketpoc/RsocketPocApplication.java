846df12c651f2fc8bcee2288092bbc97a5de9e93
김우재 <kwj1270@estsoft.com>
2022-08-29 15:40:53 +0900

    feat(kwj120): initialize project

---
+++ src/main/java/com/poc/rsocketpoc/RsocketPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.rsocketpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poc/rsocketpoc/RsocketPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.rsocketpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
